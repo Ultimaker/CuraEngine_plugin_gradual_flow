fb306c96710ad45b2edaad00b1b4e7f9e7e6880a
Jelle Spijker <j.spijker@ultimaker.com>
2023-08-31 07:25:01 +0200

    bump up catch2
    catch 3.1.0 has a bug when compiling with gcc-13
    
    Contributes to CURA-10951

--- conanfile.py
+++ conanfile.py
@@ -174,7 +174,7 @@ class CuraEngineGradualFlowPluginConan(ConanFile):
     def build_requirements(self):
         self.test_requires("standardprojectsettings/[>=0.1.0]@ultimaker/stable")
         if not self.conf.get("tools.build:skip_test", False, check_type=bool):
-            self.test_requires("catch2/[>=2.13.8]")
+            self.test_requires("catch2/[>=3.4.0]")
 
     def validate(self):
         # validate the minimum cpp standard supported. For C++ projects only
